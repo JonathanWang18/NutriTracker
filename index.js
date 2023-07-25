475c7d7ee36a78fc512dc2d5ffdc678647465d8c
Jonathan Wang <wangjonathan18@gmail.com>
2023-07-24 22:30:43 -0700

    Reedited cuttingboard with clipping textures

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const app = express()
 const path = require('path')
 const port = 3000
 const search = require("./routes/search.js")
-var mustacheExpress = require('mustache-express')
 require('dotenv').config()
 
-app.engine('mustache', mustacheExpress())
-app.set('view engine', 'mustache')
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/foodsearch', search)
 app.get('/', (req, res) => {
-  res.render('index');
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 
 })
 
