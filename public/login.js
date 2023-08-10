d661a679481cbd841abedacbbe9131c29c24694c
Jonathan Wang <wangjonathan18@gmail.com>
2023-08-09 20:19:56 -0700

    Fixed some styling

--- public/login.js
+++ public/login.js
@@ -1,4 +1,26 @@
-function exitLogin() {
-    document.getElementsByClassName("popup").style.visibility = "hidden";
+var loginOpen = document.getElementById("openLogin")
+var navLogin = document.getElementById("displayLogin")
+var popup = document.getElementsByClassName("popup")[0];
+
+
+function closeLogin(event) {
+    event.preventDefault();
+    popup.style.display = "none";
 }
 
+function openLogin(event) {
+    event.preventDefault();
+    popup.style.display = "block";
+
+}
+
+window.onclick = function (event) {
+    console.log("Click Works\n")
+    if (event.target == document.getElementsByClassName("wrap")[0]) {
+      closeLogin();
+    }
+}
+
+
+navLogin.addEventListener('click', openLogin)
+loginOpen.addEventListener('click', closeLogin)
